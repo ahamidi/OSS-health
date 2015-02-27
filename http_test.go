8a0295377e9f8e121e6a6db38b1b4d814b06105c
Ali Hamidi <ahamidi@me.com>
2015-02-27 09:14:43 -0800

    rewrote test

--- http_test.go
+++ http_test.go
@@ -1,11 +1,14 @@
 package main
 
 import (
-	"fmt"
+	//"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 var (
@@ -15,30 +18,40 @@ var (
 )
 
 func init() {
-	server = httptest.NewServer(handler) //Creating new server with the user handlers
+	//server = httptest.NewServer(handler) //Creating new server with the user handlers
 
-	usersUrl = fmt.Sprintf("%s/users", server.URL) //Grab the address for the API endpoint
+	//usersUrl = fmt.Sprintf("%s/users", server.URL) //Grab the address for the API endpoint
 }
 
 func TestValidPath(t *testing.T) {
-	test := "140proof/OSS-Health"
+
+	in := "140proof/OSS-Health"
+	out := string(`owner:140proof project:OSS-Health grade:%100`)
 	resp := httptest.NewRecorder()
 
-	req, err := http.NewRequest("GET", "localhost:8080/"+test, nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	//t.Fail()
-}
-
-func TestInvalidPath(t *testing.T) {
-	test := "140proof/OSS-Health"
-	resp := httptest.NewRecorder()
-
-	req, err := http.NewRequest("GET", "localhost:8080/"+test, nil)
+	req, err := http.NewRequest("GET", "http://localhost:8080/"+in, nil)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	//t.Fail()
+	m := handlers()
+	m.ServeHTTP(resp, req)
+
+	if o, err := ioutil.ReadAll(resp.Body); err != nil {
+		t.Fail()
+	} else {
+		assert.Equal(t, out, string(o))
+	}
 }
+
+//func TestInvalidPath(t *testing.T) {
+//test := "140proof/OSS-Health"
+//resp := httptest.NewRecorder()
+
+//req, err := http.NewRequest("GET", "localhost:8080/"+test, nil)
+//if err != nil {
+//t.Fatal(err)
+//}
+
+////t.Fail()
+//}
