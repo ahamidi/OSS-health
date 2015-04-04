5fc0fa6c27e0deee09f0935dd132153ea7aca016
Shawn O'Hare <ohare.shawn@gmail.com>
2015-04-04 15:01:55 -0700

    Get participation stats from repo.

--- activitystats.go
+++ activitystats.go
@@ -1,7 +1,12 @@
 package main
 
-import "log"
+import (
+	"log"
 
+	"github.com/google/go-github/github"
+)
+
+// CommitStats is a summary of the last year's worth of commit activity.
 type CommitStats struct {
 	//Day     int
 	Week    int
@@ -10,6 +15,14 @@ type CommitStats struct {
 	Year    int
 }
 
+// ParticipationStats contains a summary of commit activity for the repo
+// by everyone and by the owner only.
+type ParticipationStats struct {
+	All   *CommitStats
+	Owner *CommitStats
+}
+
+// FIXME can we delete? Output subsumed by GetParticipationStats
 func GetCommitStats(owner, repoName string) *CommitStats {
 
 	commitStats := &CommitStats{}
@@ -39,4 +52,41 @@ func GetCommitStats(owner, repoName string) *CommitStats {
 	commitStats.Year = curSumTotal
 
 	return commitStats
+}
+
+func GetParticipationStats(owner, repoName string) *ParticipationStats {
+	parStats := &ParticipationStats{
+		All:   &CommitStats{},
+		Owner: &CommitStats{},
+	}
+
+	par, _, err := context.client.Repositories.ListParticipation(owner, repoName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	l := len(par.All) - 1 // to traverse backwards
+	allSum := 0           // running commit count for everyone
+	ownerSum := 0         // running commit count for owner
+	for i := range par.All {
+		allSum += par.All[l-i]
+		ownerSum += par.Owner[l-i]
+
+		switch i {
+		case 1:
+			parStats.All.Week = allSum
+			parStats.Owner.Week = ownerSum
+		case 4:
+			parStats.All.Month = allSum
+			parStats.Owner.Month = ownerSum
+		case 12:
+			parStats.All.Quarter = allSum
+			parStats.Owner.Quarter = ownerSum
+		}
+	}
+
+	parStats.All.Year = allSum
+	parStats.Owner.Year = ownerSum
+
+	return parStats
 }
